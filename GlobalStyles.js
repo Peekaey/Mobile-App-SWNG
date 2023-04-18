7386ff64f9f15ee977b7ed8c7112bd54093760cb
natashaSharma09 <64687920+natashaSharma09@users.noreply.github.com>
2023-04-18 23:05:05 +1000

    Add files via upload

---
+++ GlobalStyles.js
@@ -0,0 +1,37 @@
+/* fonts */
+export const FontFamily = {
+  robotoBold: "Roboto_bold",
+  robotoRegular: "Roboto_regular",
+  poppinsBold: "Poppins_bold",
+  poppinsMedium: "Poppins_medium",
+  poppinsSemibold: "Poppins_semibold",
+  rubikRegular: "Rubik_regular",
+  rubikBold: "Rubik_bold",
+};
+/* font sizes */
+export const FontSize = {
+  size_xl: 20,
+  size_smi: 13,
+  size_lgi_1: 19,
+  size_sm: 14,
+  size_mini: 15,
+  size_3xl: 22,
+};
+/* Colors */
+export const Color = {
+  white: "#fff",
+  firebrick: "#cf2e2e",
+  darkgray: "#969696",
+  whitesmoke: "#f6f6f6",
+  black: "#000",
+  dimgray: "#4e4949",
+  gray: "#898989",
+  darkslategray: "#1c2d40",
+  gainsboro: "#d9d9d9",
+};
+/* border radiuses */
+export const Border = {
+  br_3xs: 10,
+  br_23xl: 42,
+  br_sm: 14,
+};

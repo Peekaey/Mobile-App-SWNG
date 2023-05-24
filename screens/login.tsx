6f14cfd4e08e77974d84fa96938629216ba3cc28
Dallas <dallasjt31@gmail.com>
2023-05-25 07:48:08 +1000

    Updated Login to include RememeberPassword functionality

--- screens/login.tsx
+++ screens/login.tsx
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { Text, View } from '../components/Themed';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScreenStackHeaderSearchBarView } from 'react-native-screens';
 import { Navigator } from 'expo-router';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
@@ -15,7 +15,7 @@ import Nav from '../App';
 import axios from 'axios';
 
 import * as Notifications from 'expo-notifications';
-
+import { FontAwesome } from '@expo/vector-icons'; // Import the required icon library
 
 const enableForegroundNotifications = async () => {
   let settings = await Notifications.getPermissionsAsync();
@@ -92,12 +92,16 @@ const scheduleLocalNotification = async () => {
 scheduleLocalNotification();
 
 
+var defaultUsername:any;
+var defaultPassword:any;
 
 
 export default function LoginScreen() {
 
 
 
+
+  
 
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
   navigation.getParent()?.setOptions({tabBarStyle: {display: 'none'}});
@@ -108,6 +112,33 @@ export default function LoginScreen() {
   const [errorMessage, setError] = useState('');
 
   const [isLoading, setLoading] = useState(false); // New state variable
+
+  const [rememberPassword, setRememberPassword] = useState(false);
+
+
+  useEffect(() => {
+    const checkStoredLoginCredentials = async () => {
+      const RememberPassword = await AsyncStorage.getItem('RememberPassword');
+
+      if (RememberPassword === 'true') {
+        const storedUsername = await AsyncStorage.getItem('LongTermUsername');
+        const storedPassword = await AsyncStorage.getItem('LongTermPassword');
+        if (storedUsername && storedPassword) {
+          setUsername(storedUsername);
+          setPassword(storedPassword);
+          setRememberPassword(true); // Set rememberPassword to true if credentials are found
+        }
+      } else {
+        setUsername('');
+        setPassword('');
+      }
+    };
+
+    checkStoredLoginCredentials();
+  }, []); // Empty dependency array to run the effect only once
+
+
+
 
 
   const handleLogin = async () => {
@@ -130,7 +161,40 @@ export default function LoginScreen() {
       console.log('Stored Token:', storedToken);
       console.log('Stored User ID:', storedUserId);
 
-      
+      if (rememberPassword == true) {
+        console.log("Storing Password Long Term");
+        
+        await AsyncStorage.setItem('LongTermUsername', username);
+        await AsyncStorage.setItem('LongTermPassword', password);
+        await AsyncStorage.setItem('RememberPassword', rememberPassword.toString())
+
+        const LongTermUsername = await AsyncStorage.getItem('LongTermUsername');
+        const LongTermPassword = await AsyncStorage.getItem('LongTermPassword');
+        const RememberPassword = await AsyncStorage.getItem('RememberPassword')
+        console.log('Stored Username:', LongTermUsername);
+        console.log('Stored Password:', LongTermPassword);
+        console.log('Remember Password Stored valuje:', RememberPassword)
+      }
+
+      if (rememberPassword == false) {
+        console.log("Removing Password Long Term");
+        
+        await AsyncStorage.removeItem('LongTermUsername');
+        await AsyncStorage.removeItem('LongTermPassword');
+
+        await AsyncStorage.setItem('RememberPassword', rememberPassword.toString())
+
+        const LongTermUsername = await AsyncStorage.getItem('LongTermUsername');
+        const LongTermPassword = await AsyncStorage.getItem('LongTermPassword');
+        const RememberPassword = await AsyncStorage.getItem('RememberPassword')
+        console.log('Stored Username:', LongTermUsername);
+        console.log('Stored Password:', LongTermPassword);
+        console.log('Remember Password Stored valuje:', RememberPassword)
+      }
+
+
+
+
 
       // Perform any necessary actions after successful authentication, such as navigating to a new screen
 
@@ -145,6 +209,8 @@ export default function LoginScreen() {
 
   };
 
+  const usernamePlaceholder = rememberPassword ? '' : 'Username';
+  const passwordPlaceholder = rememberPassword ? '' : 'Password';
 
   return (
     <View style={styles.container}>
@@ -153,18 +219,36 @@ export default function LoginScreen() {
       <Text></Text>
       {errorMessage !== '' && <Text style={styles.errorMessage}>{errorMessage}</Text>}
       <TextInput
-        placeholder="Username"
+        placeholder={`${usernamePlaceholder}`}
         style={styles.input}
         value={username}
         onChangeText={(text) => setUsername(text)}
       />
       <TextInput
-        placeholder="Password"
+        placeholder={`${passwordPlaceholder}`}
         style={styles.input}
         value={password}
         onChangeText={(text) => setPassword(text)}
         secureTextEntry // Use asterisks instead of plain text
       />
+  
+  <View style={styles.checkboxContainer}>
+        <Text style={styles.checkboxLabel}>Remember Password  </Text>
+        <TouchableOpacity onPress={() => setRememberPassword(!rememberPassword)}>
+          <View
+            style={[
+              styles.checkbox,
+              rememberPassword ? styles.checkedBox : styles.uncheckedBox,
+            ]}
+          >
+            {rememberPassword ? (
+              <FontAwesome name="check" color="white" size={12} style={styles.checkboxTick} />
+            ) : null}
+          </View>
+        </TouchableOpacity>
+      </View>
+
+
       <TouchableOpacity
         style={[styles.button, isLoading && styles.disabledButton]} // Disable button style if loading
         onPress={handleLogin}
@@ -246,6 +330,35 @@ const styles = StyleSheet.create({
   },
   disabledButton: {
     backgroundColor: '#8B0000',
-  }
+  },
+
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  checkbox: {
+    width: 20,
+    height: 20,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  checkedBox: {
+    backgroundColor: 'brown',
+  },
+  uncheckedBox: {
+    backgroundColor: 'transparent',
+  },
+  checkboxLabel: {
+
+    marginLeft: 8,
+  },
+  checkboxTick: {
+    top: 1, // Adjust this value to vertically center the tick icon
+  },
+
 });
 

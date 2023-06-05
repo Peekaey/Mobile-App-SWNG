595dab259cd2d992862c39194f42fdd05124d9bb
Peekaey <dallasjt31@gmail.com>
2023-06-05 19:55:08 +1000

    Updated bunch of files

--- components/checkTokenStatus.tsx
+++ components/checkTokenStatus.tsx
@@ -23,51 +23,96 @@ import Toast from 'react-native-toast-message';
 
 // Main function to check status of a session token
 // If Valid = No Action - If invalid = return to login page and display error to tell user to log in again.
-export default function CheckTokenStatus() {
+export default function updateProfile() {
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
 
-    // Declaring navigation dependencies
-    const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
-  
-    // Grabs current session token - if non exists = return to login page
-    useEffect(() => {
-      const validateToken = async () => {
-        try {
-          const token = await SecureStore.getItemAsync('token');
-          if (!token) {
-            navigation.navigate('Login');
-            Alert.alert('Session Expired', 'Your session token is invalid or expired, please login again');
-          }
-          
-          // Add headers to POST request before validating token
-          const axiosInstance = axios.create({
-            headers: {
-              Authorization: `Bearer ${token}`,
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-              'Accept-Encoding': 'gzip, deflate, br',
-              'User-Agent': 'Your-User-Agent',
-            },
-          });
-  
-          // Sending POST Request to token endpoint
-          const response = await axiosInstance.post('https://swng.org.au/wp-json/jwt-auth/v1/token/validate');
-          if (response.data.code === 'jwt_auth_valid_token' && response.data.data.status === 200) {
-            // If token valid - do nothing
-            console.log('Token is valid');
-          } else {
-            // If token invalid/expired - return to login page and display alert
-            navigation.navigate('Login');
-            Alert.alert('Session Expired', 'Your session token is invalid or expired, please login again');
-          }
-        } catch (error) {
+  useEffect(() => {
+    const validateToken = async () => {
+      try {
+        const token = await SecureStore.getItemAsync('token');
+        if (!token) {
+          navigation.navigate('Login');
+          Alert.alert('Session Expired', 'Your session token is invalid or expired, please login again');
+        }
+
+        const axiosInstance = axios.create({
+          headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'User-Agent': 'Your-User-Agent',
+          },
+        });
+
+        const response = await axiosInstance.post('https://swng.org.au/wp-json/jwt-auth/v1/token/validate');
+        if (response.data.code === 'jwt_auth_valid_token' && response.data.data.status === 200) {
+          console.log('Token is valid');
+          updateProfile();
+        } else {
+          navigation.navigate('Login');
+          Alert.alert('Session Expired', 'Your session token is invalid or expired, please login again');
+        }
+      } catch (error) {
+        navigation.navigate('Login');
+        Alert.alert('Session Expired', 'Your session token is invalid or expired, please login again');
+      }
+    };
+
+    validateToken();
+  }, []);
+
+  return null;
+}
+
+
+// Main function to check status of a session token
+// If Valid = No Action - If invalid = return to login page and display error to tell user to log in again.
+export default function CheckTokenStatusOnPageLoad() {
+
+  // Declaring navigation dependencies
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+  // Grabs current session token - if non exists = return to login page
+  useEffect(() => {
+    const validateToken = async () => {
+      try {
+        const token = await SecureStore.getItemAsync('token');
+        if (!token) {
+          navigation.navigate('Login');
+          Alert.alert('Session Expired', 'Your session token is invalid or expired, please login again');
+        }
+
+        // Add headers to POST request before validating token
+        const axiosInstance = axios.create({
+          headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'User-Agent': 'Your-User-Agent',
+          },
+        });
+
+        // Sending POST Request to token endpoint
+        const response = await axiosInstance.post('https://swng.org.au/wp-json/jwt-auth/v1/token/validate');
+        if (response.data.code === 'jwt_auth_valid_token' && response.data.data.status === 200) {
+          // If token valid - do nothing
+          console.log('Token is valid');
+        } else {
           // If token invalid/expired - return to login page and display alert
           navigation.navigate('Login');
           Alert.alert('Session Expired', 'Your session token is invalid or expired, please login again');
         }
-      };
-  
-      validateToken();
-    }, []);
-  
-    return null;
-  }
+      } catch (error) {
+        // If token invalid/expired - return to login page and display alert
+        navigation.navigate('Login');
+        Alert.alert('Session Expired', 'Your session token is invalid or expired, please login again');
+      }
+    };
+
+    validateToken();
+  }, []);
+
+  return null;
+}
--- screens/profile.tsx
+++ screens/profile.tsx
@@ -1,35 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, Modal, TextInput, TouchableOpacity, ImageBackground, KeyboardAvoidingView, } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  Modal,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Alert,
+} from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 import { Text, View } from '../components/Themed';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
+import UpdateProfileCheck from "../components/checkTokenStatus";
+import CheckTokenStatusOnPageLoad from "../components/checkTokenStatus";
+import userAvatar from '../assets/userAvatar.png'
+import {ParamListBase, useNavigation} from "@react-navigation/native";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import axios from "axios";
 
-// const for pop-up
-const MyModal = ({visible, onClose}) => {
-  return (
-    <Modal animationType="slide" transparent={true} visible={visible} onRequestClose={onClose}>
-      <View style={styles.popUpContainer}>
-        <View style={styles.popUpTextContainer}>
-          <Text style={styles.popUpText}>Your Profile is updated</Text>
-          <TouchableOpacity onPress={onClose}>
-            <Text style={styles.popUpButton}>RETURN</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </Modal>
-  );
-};
 
-// const for update data 
+
+async function getAvatar() {
+  let storedAvatarURL = await SecureStore.getItemAsync('avatarURL');
+
+  if (storedAvatarURL === null) {
+    console.log("Profile Photo Local Storage is empty");
+    storedAvatarURL = '';
+  } else {
+    console.log("Profile Photo Local Storage is not empty and is:", storedAvatarURL);
+  }
+
+  return storedAvatarURL;
+}
+
+
+
+// const for update data
 const updateUserProfile = async (user_name, user_url, user_description) => {
 
   const storedToken = await SecureStore.getItemAsync('token');
   const storedUserId = await SecureStore.getItemAsync('user_id');
 
- // console.log ("PROFILE SCREEN STORED USER ID", storedUserId)
- // console.log ("PROFILE SCREEN STORED token", storedToken)
-  
+  // console.log ("PROFILE SCREEN STORED USER ID", storedUserId)
+  // console.log ("PROFILE SCREEN STORED token", storedToken)
+
+
   const end_point = `https://swng.org.au/wp-json/wp/v2/users/${storedUserId}`;
   const data ={};
   if(user_name){
@@ -41,103 +58,229 @@ const updateUserProfile = async (user_name, user_url, user_description) => {
   if(user_description){
     data.description = user_description;
   }
- 
+
   const options = {
-  method: 'PUT',
-  headers: {
-  Authorization: `Bearer ${storedToken}`,
-  'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${storedToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
   };
   const response = await fetch(end_point, options);
   const result = await response.json();
   return result;
-  };
+};
+
+
+
+
+
 
 export default function ProfilePage() {
 
-    const [isUpdated, setIsUpdated] = useState(false);
-    const [user_name, setName] = useState('');
-    const [user_description, setdescription] = useState('');
-    const[checkValidURL, setcheckValidURL] = useState(false);
-    const [user_url, setUrl] = useState('');
-    
-    //Variable for regx validation 
-    const is_URL_Wrong = checkValidURL;
-    const is_URL_empty = user_url;
-    const buttonBackgroundColor = is_URL_Wrong? '#c11717' : '#ed3434';
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
 
-    const updateinfo = async () => {
+
+CheckTokenStatusOnPageLoad();
+
+
+  const [avatarUrl, setAvatarUrl] = useState<string | undefined>(undefined);
+  useEffect(() => {
+    const fetchAvatar = async () => {
+      const storedAvatarURL = await getAvatar();
+
+      if (storedAvatarURL === '') {
+        setAvatarUrl(undefined);
+      } else {
+        setAvatarUrl(storedAvatarURL);
+      }
+    };
+
+    fetchAvatar();
+  }, []);
+
+
+
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [user_name, setName] = useState('');
+  const [user_description, setdescription] = useState('');
+  const[checkValidURL, setcheckValidURL] = useState(false);
+  const [user_url, setUrl] = useState('');
+
+  const [isAnyFieldFilled, setIsAnyFieldFilled] = useState(false);  //Variable for regx validation
+  const is_URL_Wrong = checkValidURL;
+  const is_URL_empty = user_url;
+  const buttonBackgroundColor = is_URL_Wrong || !isAnyFieldFilled ? '#8B0000' : '#ed3434';
+  const [checkToken, setCheckToken] = useState(false);
+  const [ValidatedToken, setValidatedToken] = useState(false);
+
+
+  function updateProfile() {
+
+    const validateToken = async () => {
+      try {
+        const token = await SecureStore.getItemAsync('token');
+        if (!token) {
+          navigation.navigate('Login');
+          Alert.alert('Session Expired', 'Your session token is invalid or expired, please login again');
+        }
+        const axiosInstance = axios.create({
+          headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'User-Agent': 'Your-User-Agent',
+          },
+        });
+
+        const response = await axiosInstance.post('https://swng.org.au/wp-json/jwt-auth/v1/token/validate');
+        if (response.data.code === 'jwt_auth_valid_token' && response.data.data.status === 200) {
+          console.log('Token is valid');
+          await updateinfo();
+        } else {
+          navigation.navigate('Login');
+          Alert.alert('Session Expired', 'Your session token is invalid or expired, please login again');
+        }
+      } catch (error) {
+        navigation.navigate('Login');
+        Alert.alert('Session Expired', 'Your session token is invalid or expired, please login again');
+      }
+    };
+
+    validateToken();
+
+    return null;
+  }
+
+  const updateinfo = async () => {
     const result = await updateUserProfile(user_name, user_url, user_description); //call updateUserProfile function
-     console.log(result);
+    console.log(result);
 
     setIsUpdated(true);
     setName('');
     setUrl('');
     setdescription('');
-    };
+    setcheckValidURL(false);
+    setIsAnyFieldFilled(false);
+  };
 
-    //function for regx validation
-    const handleCheckURL = text => {
-      let regx = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/im;
-      setUrl(text);
-      if (regx.test(text)) {
-        setcheckValidURL(false);
-      }
-       else {
-        setcheckValidURL(true);
-      }
-    };
 
-    // check if the url input box is empty 
-    const handleBlur = () => {
-      if(is_URL_empty == ""){
+
+
+
+  useEffect(() => {
+    if (isUpdated) {
+      Alert.alert(
+          'Update Successful',
+          'Your profile has been updated successfully.',
+          [
+            {
+              text: 'OK',
+              onPress: () => setIsUpdated(false),
+            },
+          ]
+      );
+    }
+  }, [isUpdated]);
+  //function for regx validation
+  const handleCheckURL = text => {
+    let regx = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/im;
+    setUrl(text);
+    if (regx.test(text)) {
+      setcheckValidURL(false);
+    }
+    else {
+      setcheckValidURL(true);
+    }
+  };
+
+  // check if the url input box is empty
+  const handleBlur = () => {
+    if(is_URL_empty == ""){
       setcheckValidURL("");
-      }
-    };
+    }
+  };
 
-    return (
+  return (
 
-    <View style={styles.container}>
-
-      <View style={styles.centeredContainer}>
-        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      </View>
+      <View style={styles.container}>
 
         <View style={styles.centeredContainer}>
-        <Image source={require('../assets/userAvatar.png')} style={styles.image} />
-        <Text style={styles.ProfilePhotoAnchorText}> Edit ✎</Text>
-        </View>
-
-        <MyModal visible={isUpdated} onClose={() => setIsUpdated(false)} />
-        <View style={styles.centeredContainer}>
-        <Text style={styles.textboxAnchorText}>Business Name</Text>
-        <TextInput placeholder="Enter business name"  onChangeText={setName} style={styles.input}></TextInput>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
         </View>
 
         <View style={styles.centeredContainer}>
-        <Text style={styles.textboxAnchorText}>Business Website URL</Text>
-        <TextInput placeholder="https://example.com" value={user_url} onChangeText={text => handleCheckURL(text)} 
-        onBlur={handleBlur} style={styles.input}></TextInput>
+          {avatarUrl ? (
+              <Image source={{ uri: avatarUrl }} style={styles.image} />
+          ) : (
+              <View>
+                <Image
+                    source={userAvatar}
+                    style={styles.image}
+                />
+              </View>
+          )}
+        </View>
+
+
+        <View style={styles.centeredContainer}>
+          <Text style={styles.ProfilePhotoAnchorText}> Edit ✎ </Text>
+
+          <Text style={styles.textboxAnchorText}>Business Name</Text>
+          <TextInput
+              placeholder="Enter business name"
+              onChangeText={(text) => {
+                setName(text);
+                setIsAnyFieldFilled(!!text);
+              }}
+              style={styles.input}
+          />
+        </View>
+
+        <View style={styles.centeredContainer}>
+          <Text style={styles.textboxAnchorText}>Business Website URL</Text>
+          <TextInput
+              placeholder="https://example.com"
+              value={user_url}
+              onChangeText={(text) => {
+                handleCheckURL(text);
+                setIsAnyFieldFilled(!!text);
+              }}
+              onBlur={handleBlur}
+              style={styles.input}
+          />
         </View>
         {checkValidURL? (
-          <Text style={styles.errorMessage}>Please enter a valid URL</Text>
+            <Text style={styles.errorMessage}>Please enter a valid URL</Text>
         ) : (
-          <Text style={styles.errorMessage}></Text>
+            <Text style={styles.errorMessage}></Text>
         )}
 
         <View style={styles.centeredContainer}>
-        <Text style={styles.textboxAnchorText}>Company Description</Text>
-        <TextInput placeholder="Enter company description" value={user_description} onChangeText={setdescription} style={styles.input}></TextInput>
+          <Text style={styles.textboxAnchorText}>Company Description</Text>
+          <TextInput
+              placeholder="Enter company description"
+              value={user_description}
+              onChangeText={(text) => {
+                setdescription(text);
+                setIsAnyFieldFilled(!!text);
+              }}
+              style={styles.input}
+          />
         </View>
 
         <View style={styles.centeredContainer}>
-        <TouchableOpacity style={[styles.button, { backgroundColor: buttonBackgroundColor }]} disabled={is_URL_Wrong} onPress={updateinfo}>
-        <Text style={styles.buttonText} >Update Profile</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+              style={[styles.button, { backgroundColor: buttonBackgroundColor }]}
+              disabled={checkValidURL || !isAnyFieldFilled}
+              onPress={updateProfile}
+          >
+            <Text style={styles.buttonText}>Update Profile</Text>
+          </TouchableOpacity>
         </View>
-    </View>
+      </View>
   );
 }
 
@@ -152,7 +295,7 @@ const styles = StyleSheet.create({
   image: {
     resizeMode: 'contain',
     height: 100,
-    width: 100, 
+    width: 100,
   },
   title: {
     fontSize: 20,
@@ -193,6 +336,7 @@ const styles = StyleSheet.create({
     marginBottom: '2%',
   },
   ProfilePhotoAnchorText: {
+    marginTop: '10%',
     marginBottom: '5%'
   },
   errorMessage: {
@@ -206,13 +350,13 @@ const styles = StyleSheet.create({
   },
   //pop-up css
   popUpContainer:{
-    flex: 1, 
-    justifyContent: 'center', 
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
   },
   popUpTextContainer:{
-    backgroundColor: '#fff', 
-    padding: 20, 
+    backgroundColor: '#fff',
+    padding: 20,
   },
   popUpText:{
     fontSize: 28,

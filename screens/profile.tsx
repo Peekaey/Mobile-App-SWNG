68a4cfecf67991038274c887fea3607b50b62921
Naitik0121 <20018450@student.westernsydney.edu.au>
2023-06-07 16:08:52 +1000

    Backend_RAP_Referral_and_profile

--- screens/profile.tsx
+++ screens/profile.tsx
@@ -1,144 +1,243 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, Modal, TextInput, TouchableOpacity, ImageBackground, KeyboardAvoidingView, } from 'react-native';
-import * as SecureStore from 'expo-secure-store';
-import { Text, View } from '../components/Themed';
-import React, {useState} from 'react';
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View} from '../components/Themed';
+import { SelectList } from 'react-native-dropdown-select-list';
+import React, { useState } from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
+import * as SecureStore from 'expo-secure-store';
 
-// const for pop-up
-const MyModal = ({visible, onClose}) => {
+const Chapters_Members = () => {
+  const [selected, setSelected] = React.useState("");
+  const [chapterNames, setChapterNames] = React.useState([]);
+  const [memberNames, setMemberNames] = React.useState([]);
+
+  React.useEffect(() => {
+      async function fetchChapterNames() {
+          try {
+              const response = await fetch(
+                  "https://www.swng.org.au/wp-json/swng-app/v1/chapterNames"
+              );
+              const data = await response.json();
+              const names = Object.values(data);
+              setChapterNames(names);
+          } catch (error) {
+              console.error(error);
+          }
+      }
+      fetchChapterNames();
+  }, []);
+
+  React.useEffect(() => {
+      async function fetchMemberNames() {
+          try {
+              if (selected) {
+                  const response = await fetch(
+                      `https://www.swng.org.au/wp-json/swng-app/v1/memberNames/${selected}`
+                  );
+                  const data = await response.json();
+                  const names = Object.values(data).map((member) => member.name);
+                  setMemberNames(names);
+              } else {
+                  setMemberNames([]);
+              }
+          } 
+          catch (error) {
+              console.error(error);
+          }
+      }
+      fetchMemberNames();
+  }, [selected]);
+
+  const chapterData = chapterNames.slice(1).map((name, index) => ({
+      key: index.toString(),
+      value: name,
+  }));
+
+  const memberData = memberNames.map((name, index) => ({
+      key: index.toString(),
+      value: name,
+  }));
+
+  const handleSelectChapter = (selectedOption) => {
+      const selectedChapterName = chapterData.find(
+          (option) => option.key === selectedOption
+      )?.value;
+      setSelected(selectedChapterName);
+  };
+
+  const handleSelectMember = (selectedOption) => {
+      const selectedMemberName = memberData.find(
+          (option) => option.key === selectedOption
+      )?.value;
+  };
+
   return (
-    <Modal animationType="slide" transparent={true} visible={visible} onRequestClose={onClose}>
-      <View style={styles.popUpContainer}>
-        <View style={styles.popUpTextContainer}>
-          <Text style={styles.popUpText}>Your Profile is updated</Text>
-          <TouchableOpacity onPress={onClose}>
-            <Text style={styles.popUpButton}>RETURN</Text>
-          </TouchableOpacity>
-        </View>
+      <View style={styles.dropdown}>
+          <View style={styles.chap_dropdown}>
+            <Text style={styles.dropDownText}>Select the Chapter:</Text>
+            <SelectList data={chapterData} setSelected={handleSelectChapter} />
+          </View>
+          <View style={styles.mem_dropdown}>
+            <Text style={styles.dropDownText}>Select the Member:</Text>
+            <SelectList data={memberData} setSelected={handleSelectMember} />
+          </View>
       </View>
-    </Modal>
   );
 };
 
-// const for update data 
-const updateUserProfile = async (user_name, user_url, user_description) => {
+// const for POST data 
+const postReferral = async (username, org, email, phonenum, notes) => {
 
-  const storedToken = await SecureStore.getItemAsync('token');
   const storedUserId = await SecureStore.getItemAsync('user_id');
+  const end_point = 'https://www.swng.org.au/wp-json/swng-app/v1/rap';
 
- // console.log ("PROFILE SCREEN STORED USER ID", storedUserId)
- // console.log ("PROFILE SCREEN STORED token", storedToken)
-  
-  const end_point = `https://swng.org.au/wp-json/wp/v2/users/${storedUserId}`;
-  const data ={};
-  if(user_name){
-    data.name = user_name;
+  const data ={
+    status_code: 200,
+    status: 'success',
+    message: 'Referral submitted'
+  };
+  if(storedUserId){
+    data.user_id = storedUserId;
   }
-  if(user_url){
-    data.url = user_url;
+  if(username){
+    data.name = username;
   }
-  if(user_description){
-    data.description = user_description;
+  if(org){
+    data.organisation = org;
+  }
+  if(phonenum){
+    data.phone = phonenum;
+  }
+  if(email){
+    data.email = email;
+  }
+  if(notes){
+    data.notes = notes;
   }
  
   const options = {
-  method: 'PUT',
-  headers: {
-  Authorization: `Bearer ${storedToken}`,
-  'Content-Type': 'application/json',
-  },
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json',
+    },
   body: JSON.stringify(data),
   };
+
   const response = await fetch(end_point, options);
   const result = await response.json();
+  const alertMess = `Status Code: ${result.status_code}\nStatus: ${result.status}\nMessage: ${result.message}`;
+  alert(alertMess);
   return result;
+};
+
+export default function ReferralsPage() {
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [username, setUsername] = React.useState('');
+  const [org, setorg] = React.useState('');
+  const [email, setemail] = React.useState('');
+  const[checkValidEmail, setCheckValidEmail] = React.useState(false);
+  const [phonenum, setphonenum] = React.useState('');
+  const[checkValidPhoneNum, setcheckValidPhoneNum] = React.useState(false);
+  const [notes, setnotes] = React.useState('');
+
+  const isAnyFieldEmpty = !username || checkValidPhoneNum || checkValidEmail || !notes;
+  const buttonBackgroundColor = isAnyFieldEmpty ? '#c11717' : '#ed3434';
+  const isPhoneEmpty = phonenum;
+
+  const handleCheckEmail = text => {
+    let re = /\S+@\S+\.\S+/;
+    let regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+    setemail(text);
+    if (re.test(text) || regex.test(text)) {
+      setCheckValidEmail(false);
+    } else {
+      setCheckValidEmail(true);
+    }
   };
 
-export default function ProfilePage() {
+  const handleCheckPhoneNum = Value => {
+    let regex1 = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
 
-    const [isUpdated, setIsUpdated] = useState(false);
-    const [user_name, setName] = useState('');
-    const [user_description, setdescription] = useState('');
-    const[checkValidURL, setcheckValidURL] = useState(false);
-    const [user_url, setUrl] = useState('');
-    
-    //Variable for regx validation 
-    const is_URL_Wrong = checkValidURL;
-    const is_URL_empty = user_url;
-    const buttonBackgroundColor = is_URL_Wrong? '#c11717' : '#ed3434';
+    setphonenum(Value);
+    if (regex1.test(Value)) {
+      setcheckValidPhoneNum(false);
+    } else {
+      setcheckValidPhoneNum(true);
+    }
+  };
 
-    const updateinfo = async () => {
-    const result = await updateUserProfile(user_name, user_url, user_description); //call updateUserProfile function
-     console.log(result);
+  const handleBlur = () => {
+    if(phonenum.length==0){
+    setcheckValidPhoneNum("");
+    }
+  };
+
+  const updateinfo = async () => {
+    const result = await postReferral(username, org, email, phonenum, notes); //call postReferral function
+    console.log(result);
 
     setIsUpdated(true);
-    setName('');
-    setUrl('');
-    setdescription('');
-    };
+    setUsername('');
+    setorg('');
+    setemail('');
+    setphonenum('');
+    setnotes('')
+  };
 
-    //function for regx validation
-    const handleCheckURL = text => {
-      let regx = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/im;
-      setUrl(text);
-      if (regx.test(text)) {
-        setcheckValidURL(false);
-      }
-       else {
-        setcheckValidURL(true);
-      }
-    };
+return (
+  <View style={styles.container}>
+    <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
 
-    // check if the url input box is empty 
-    const handleBlur = () => {
-      if(is_URL_empty == ""){
-      setcheckValidURL("");
-      }
-    };
-
-    return (
-
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={{ flexGrow: 1, paddingBottom: 100 }} automaticallyAdjustKeyboardInsets={true}>
+    
+      <Chapters_Members/>
 
       <View style={styles.centeredContainer}>
-        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <Text style={styles.textboxAnchorText}>Name(required):</Text>
+        <TextInput placeholder="Enter Name" value={username} onChangeText={text => setUsername(text)} style={styles.input1}></TextInput>
       </View>
 
-        <View style={styles.centeredContainer}>
-        <Image source={require('../assets/userAvatar.png')} style={styles.image} />
-        <Text style={styles.ProfilePhotoAnchorText}> Edit ✎</Text>
-        </View>
+      <View style={styles.centeredContainer}>
+        <Text style={styles.textboxAnchorText}>Business/Organisation:</Text>
+        <TextInput placeholder=" Enter Business/Organisation" value={org} onChangeText={text => setorg(text)} style={styles.input1}></TextInput>
+      </View>
 
-        <MyModal visible={isUpdated} onClose={() => setIsUpdated(false)} />
-        <View style={styles.centeredContainer}>
-        <Text style={styles.textboxAnchorText}>Business Name</Text>
-        <TextInput placeholder="Enter business name"  onChangeText={setName} style={styles.input}></TextInput>
-        </View>
-
-        <View style={styles.centeredContainer}>
-        <Text style={styles.textboxAnchorText}>Business Website URL</Text>
-        <TextInput placeholder="https://example.com" value={user_url} onChangeText={text => handleCheckURL(text)} 
-        onBlur={handleBlur} style={styles.input}></TextInput>
-        </View>
-        {checkValidURL? (
-          <Text style={styles.errorMessage}>Please enter a valid URL</Text>
+      <View style={styles.centeredContainer}>
+        <Text style={styles.textboxAnchorText}>Phone:</Text>
+        <TextInput placeholder="0000000000" value={phonenum} onChangeText={text => handleCheckPhoneNum(text)} onBlur={handleBlur} style={styles.input1}></TextInput>
+      </View>
+      <View style={styles.errorContainer}>
+        {checkValidPhoneNum ? (
+          <Text style={styles.errorMessage}>Please enter a valid Phone number</Text>
         ) : (
-          <Text style={styles.errorMessage}></Text>
+          <Text style={styles.errorMessage}> </Text>
         )}
+      </View>
 
-        <View style={styles.centeredContainer}>
-        <Text style={styles.textboxAnchorText}>Company Description</Text>
-        <TextInput placeholder="Enter company description" value={user_description} onChangeText={setdescription} style={styles.input}></TextInput>
-        </View>
+      <View style={styles.centeredContainer}>
+        <Text style={styles.textboxAnchorText}>Email(required):</Text>
+        <TextInput placeholder="someone@example.com" value={email} onChangeText={text => handleCheckEmail(text)} style={styles.input1}></TextInput>
+      </View>
+      <View style={styles.errorContainer}>
+        {checkValidEmail ? (
+          <Text style={styles.errorMessage}>Please enter a valid email address</Text>
+        ) : (
+          <Text style={styles.errorMessage}> </Text>
+        )}
+      </View>
 
-        <View style={styles.centeredContainer}>
-        <TouchableOpacity style={[styles.button, { backgroundColor: buttonBackgroundColor }]} disabled={is_URL_Wrong} onPress={updateinfo}>
-        <Text style={styles.buttonText} >Update Profile</Text>
-        </TouchableOpacity>
-        </View>
-    </View>
-  );
+      <View style={styles.centeredContainer}>
+        <Text style={styles.textboxAnchorText}>Notes(required):</Text>
+        <TextInput placeholder=" " value={notes} onChangeText={text => setnotes(text)} style={styles.input1}></TextInput>
+      </View>
+
+      <TouchableOpacity 
+        style={[styles.button, { backgroundColor: buttonBackgroundColor }]} disabled={isAnyFieldEmpty} onPress={updateinfo}>
+        <Text style={styles.buttonText}>Submit Referral</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
@@ -148,79 +247,95 @@ const styles = StyleSheet.create({
   },
   centeredContainer: {
     alignItems: 'center',
+    top: 110,
+  },
+  dropdown:{
+    flexDirection: 'row',
+    alignSelf: 'flex-start',
+    marginLeft: '5%',
+    marginBottom: '2%',
+    backgroundColor: "white",
+    position: "absolute",
+    top: 30,
+    width: "100%",
+    zIndex: 1,
+   },
+   chap_dropdown:{
+    padding:5,
+    width: "45%",
+   },
+  mem_dropdown:{
+    padding:5,
+    width: "45%",
+   },
+  dropDownText:{
+    alignSelf: 'flex-start',
+    marginBottom: '2%',
+  },
+  textboxAnchorText: {
+    alignSelf: 'flex-start',
+    marginLeft: '10%',
+  },
+  input: {
+    width: '50%',
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 30,
+    alignItems: 'center',
+    textAlign: 'center',
   },
   image: {
+    marginLeft: '8%',
     resizeMode: 'contain',
-    height: 100,
-    width: 100, 
+    height: '20%',
+    width: '20%',
+    alignSelf: 'flex-start',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
   separator: {
-    marginVertical: 20,
+    marginVertical: 3,
     height: 1,
     width: '100%',
-    alignItems: 'center',
+    top:15,
   },
-  input: {
+  input1: {
     width: '80%',
-    height: 50,
+    height: 40,
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 20,
-    alignItems: 'center',
-    textAlign: 'center',
   },
   button: {
-    backgroundColor: '#ed3434',
-    width: '40%',
-    height: 50,
+    backgroundColor: '#c11717',
+    width: '30%',
+    height: 35,
     borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: '2%',
+    marginLeft: '10%',
+    top: 110,
   },
   buttonText: {
     color: '#fff',
-    fontSize: 16,
+    fontSize: 12,
     fontWeight: 'bold',
   },
-  textboxAnchorText: {
-    marginBottom: '2%',
-  },
-  ProfilePhotoAnchorText: {
-    marginBottom: '5%'
+  errorContainer: {
+    top: 90,
+    marginLeft:40,
   },
   errorMessage: {
     color: 'red',
     fontWeight: 'bold',
     fontSize: 13,
     position:'absolute',
-    margin:10,
-    top: 335,
-    marginLeft: 40,
-  },
-  //pop-up css
-  popUpContainer:{
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center',
-  },
-  popUpTextContainer:{
-    backgroundColor: '#fff', 
-    padding: 20, 
-  },
-  popUpText:{
-    fontSize: 28,
-  },
-  popUpButton:{
-    color: 'red',
-    marginTop: 20,
-    fontSize: 20,
-    marginLeft: 100,
   },
 });
--- screens/rap.tsx
+++ screens/rap.tsx
@@ -1,14 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, ScrollView, Image, TextInput, TouchableOpacity,SafeAreaView,TouchableWithoutFeedback, ImageBackground, KeyboardAvoidingView, Animated} from 'react-native';
+import { StyleSheet, ScrollView, TextInput, TouchableOpacity,SafeAreaView,TouchableWithoutFeedback, ImageBackground, KeyboardAvoidingView, Animated} from 'react-native';
 import { Text, View } from '../components/Themed';
 import { SelectList } from 'react-native-dropdown-select-list';
 import { MaterialIcons } from '@expo/vector-icons';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import * as SecureStore from 'expo-secure-store';
 
 const Star = () => {
-const starRatingOptions = [1, 2, 3, 4, 5];
-const [starRating, setStarRating] = useState(null);
+const [star_Rating, setStar_Rating] = useState(null);
 const animatedButtonScale = new Animated.Value(1);
+const starRatingArray = [1, 2, 3, 4, 5];
 
 const handlePressIn = () => {
   Animated.spring(animatedButtonScale, {
@@ -37,105 +37,188 @@ return (
     <View style={styles.container1}>
     <Text style={styles.rateheading} >Tap to Rate</Text>
       <View style={styles.stars}>
-        {starRatingOptions.map((option) => (
+        {starRatingArray.map((option) => (
           <TouchableWithoutFeedback
             onPressIn={() => handlePressIn(option)}
             onPressOut={() => handlePressOut(option)}
-            onPress={() => setStarRating(option)}
-            key={option}
-          >
+            onPress={() => setStar_Rating(option)}
+            key={option}>
+
             <Animated.View style={animatedScaleStyle}>
               <MaterialIcons
-                name={starRating >= option ? 'star' : 'star-border'}
+                name={star_Rating >= option ? 'star' : 'star-border'}
                 size={32}
-                style={starRating >= option ? styles.starSelected : styles.starUnselected}
+                style={star_Rating >= option ? styles.starSelected : styles.starUnselected}
               />
             </Animated.View>
           </TouchableWithoutFeedback>
         ))}
       </View>
-      <Text style={styles.heading}>{starRating ? `${starRating}/5` : '0/5'}</Text>
+      <Text style={styles.heading}>{star_Rating ? `${star_Rating}/5` : '0/5'}</Text>
     </View>
   </SafeAreaView>
 );
-
 };
 
-const Chapters = () => {
-
+const Chapters_Members = () => {
   const [selected, setSelected] = React.useState("");
-  
-  const data = [
-      {key:'1', value:'Chapter1'},
-      {key:'2', value:'Chapter2'},
-      {key:'3', value:'Chapter3'}
-  ];
-  return(
-    <View>
-      <Text style={styles.textboxAnchorText}> Select the Chapter:</Text>
-      <SelectList data={data} setSelected={setSelected} />
-    </View>
+  const [chapterNames, setChapterNames] = React.useState([]);
+  const [memberNames, setMemberNames] = React.useState([]);
+
+  React.useEffect(() => {
+      async function fetchChapterNames() {
+          try {
+              const response = await fetch(
+                  "https://www.swng.org.au/wp-json/swng-app/v1/chapterNames"
+              );
+              const data = await response.json();
+              const names = Object.values(data);
+              setChapterNames(names);
+          } catch (error) {
+              console.error(error);
+          }
+      }
+      fetchChapterNames();
+  }, []);
+
+  React.useEffect(() => {
+      async function fetchMemberNames() {
+          try {
+              if (selected) {
+                  const response = await fetch(
+                      `https://www.swng.org.au/wp-json/swng-app/v1/memberNames/${selected}`
+                  );
+                  const data = await response.json();
+                  const names = Object.values(data).map((member) => member.name);
+                  setMemberNames(names);
+              } else {
+                  setMemberNames([]);
+              }
+          } 
+          catch (error) {
+              console.error(error);
+          }
+      }
+      fetchMemberNames();
+  }, [selected]);
+
+  const chapterData = chapterNames.slice(1).map((name, index) => ({
+      key: index.toString(),
+      value: name,
+  }));
+
+  const memberData = memberNames.map((name, index) => ({
+      key: index.toString(),
+      value: name,
+  }));
+
+  const handleSelectChapter = (selectedOption) => {
+      const selectedChapterName = chapterData.find(
+          (option) => option.key === selectedOption
+      )?.value;
+      setSelected(selectedChapterName);
+  };
+
+  const handleSelectMember = (selectedOption) => {
+      const selectedMemberName = memberData.find(
+          (option) => option.key === selectedOption
+      )?.value;
+  };
+
+  return (
+      <View style={styles.dropdown}>
+          <View style={styles.chap_dropdown}>
+            <Text style={styles.textboxAnchorText}>Select the Chapter:</Text>
+            <SelectList data={chapterData} setSelected={handleSelectChapter} />
+          </View>
+          <View style={styles.mem_dropdown}>
+            <Text style={styles.textboxAnchorText}>Select the Member:</Text>
+            <SelectList data={memberData} setSelected={handleSelectMember} />
+          </View>
+      </View>
   );
 };
 
-const Members = () => {
+// const for POST RAP data 
+const postRap = async (subject, review, rating) => {
 
-  const [selected, setSelected] = React.useState("");
-  
-  const data = [
-      {key:'1', value:'Member1'},
-      {key:'2', value:'Member2'},
-      {key:'3', value:'Member3'}
-  ];
-  return(
-    <View>
-      <Text style={styles.textboxAnchorText}> Select the Member:</Text>
-      <SelectList data={data} setSelected={setSelected} />
-    </View>
-  );
+  const storedUserId = await SecureStore.getItemAsync('user_id');
+  const end_point = 'https://www.swng.org.au/wp-json/swng-app/v1/rap';
+
+  const data ={
+    status_code: 200,
+    status: 'success',
+    message: 'Rap submitted'
+  };
+  if(storedUserId){
+    data.user_id = storedUserId;
+  }
+  if(subject){
+    data.subject = subject;
+  }
+  if(review){
+    data.review = review;
+  }
+  if(rating){
+    data.rating = rating;
+  }
+ 
+  const options = {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json',
+    },
+  body: JSON.stringify(data),
+  };
+
+  const response = await fetch(end_point, options);
+  const result = await response.json();
+  const alertMess = `Status Code: ${result.status_code}\nStatus: ${result.status}\nMessage: ${result.message}`;
+  alert(alertMess);
+  return result;
 };
 
 export default function RapPage() {
 
+  const [isUpdated, setIsUpdated] = useState(false);
   const [subject, setsubject] = React.useState('');
   const [review, setreview] = React.useState('');
+  const [rating, setrating] = React.useState('');
 
   const isAnyFieldEmpty = !subject || !review;
   const buttonBackgroundColor = isAnyFieldEmpty ? '#c11717' : '#ed3434';
 
-  return (
+  const updateinfo = async () => {
+    const result = await postRap(subject, review, rating); //call postRap function
+    console.log(result);
 
-    
+    setIsUpdated(true);
+    setsubject('');
+    setreview('');
+    setrating('')
+  };
+
+  return (
     <View style={styles.container}>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <ScrollView contentContainerStyle={{ flexGrow: 1, paddingBottom: 100 }} automaticallyAdjustKeyboardInsets={true}>
 
-      <View style={styles.dropdown}>
-        <View style={styles.chap_dropdown}>
-          <Chapters/>
+        <Chapters_Members/> 
+        <Star/>
+
+        <View style={styles.centeredContainer}>
+        <TextInput placeholder="Enter subject" value={subject} onChangeText={text => setsubject(text)} style={styles.input}></TextInput>
         </View>
-        <View style={styles.mem_dropdown}>
-         <Members/>
-        </View>
-      </View>
-
-      <Star/>
-
-      <View style={styles.centeredContainer}>
-      <TextInput placeholder="Enter subject" value={subject} onChangeText={text => setsubject(text)} style={styles.input}></TextInput>
-      </View>
-    
-      <View style={styles.centeredContainer}>
-      <TextInput placeholder="Enter your review..."  value={review} onChangeText={text => setreview(text)} style={styles.input1}></TextInput>
-      </View>
-
       
-      <TouchableOpacity
-          style={[styles.button, { backgroundColor: buttonBackgroundColor }]}
-          disabled={isAnyFieldEmpty}>
+        <View style={styles.centeredContainer}>
+        <TextInput placeholder="Enter your review..."  value={review} onChangeText={text => setreview(text)} style={styles.input1}></TextInput>
+        </View>
+
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: buttonBackgroundColor }]} disabled={isAnyFieldEmpty} onPress={updateinfo}>
           <Text style={styles.buttonText}>Submit Review</Text>
-      </TouchableOpacity>
-        </ScrollView>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 }
@@ -157,7 +240,6 @@ const styles = StyleSheet.create({
    marginBottom: '2%',
    backgroundColor: "white",
    position: "absolute",
-   top: 40,
    width: "100%",
    zIndex: 1,
   },
@@ -169,7 +251,6 @@ const styles = StyleSheet.create({
     padding:5,
     width: "45%",
    },
-
   textboxAnchorText: {
     alignSelf: 'flex-start',
     marginBottom: '2%',
@@ -198,7 +279,7 @@ const styles = StyleSheet.create({
     borderColor: '#ccc',
     borderRadius: 5,
     paddingHorizontal: 10,
-    marginBottom: 20,
+    marginBottom: 20,  
   },
   input1: {
     width: '80%',
@@ -208,8 +289,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 20,
-  },
-  
+  }, 
   button: {
     backgroundColor: '#c11717',
     width: '30%',
@@ -225,9 +305,7 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: 'bold',
   },
-
   // star rating css
-
   rateheading:{
     fontSize: 20,
     fontWeight: 'bold',
